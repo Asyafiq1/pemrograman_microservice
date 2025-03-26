9d6ba54091ad0995e442be11cc7ce1f68b850ed5
Asyafiq1 <rizkiasyafiq11@gmail.com>
2025-03-26 23:47:40 +0700

    mikroservice

---
+++ mysql/init/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS mikroservice_produk;
+CREATE DATABASE IF NOT EXISTS mikroservice_order;
+CREATE DATABASE IF NOT EXISTS mikroservice_payment;
+CREATE DATABASE IF NOT EXISTS mikroservice_customer;
+-- Tambahkan database lain jika diperlukan
